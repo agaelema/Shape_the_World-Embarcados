c6956e8bcf9c4bb751095f7acf05b280fe794d12
Haroldo Amaral <agaelema@gmail.com>
2016-03-31 01:25:59 -0300

    Send Embarcados_00_tivaware
    Base project using TivaWare

---
+++ Projects/Embarcados_00_tivaware/main.c
@@ -0,0 +1,48 @@
+//*****************************************************************************
+//
+//	Initial Project
+//	- Based in the instructions of this book, by Stéfano Souza
+//	- http://www.embarcados.com.br/apostila-de-introducao-aos-microcontroladores-arm-cortex-m4-tiva-c/
+//
+//	Adapred by: Haroldo Amaral
+//	www.embarcados.com.br/
+//
+//	Some tips:
+//	- 	to update the library to new versions change the "TIVAWARE_INSTALL" folder destination in
+//		Resources >> Linked Resources >> Path Variables >> TIVAWARE_INSTALL
+//		CCS Build >> Variables >>  TIVAWARE_INSTALL
+//	clean the project and rebuild
+//
+//*****************************************************************************
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#include <inc/tm4c123gh6pm.h>			// include with specific hardware values
+#include "inc/hw_types.h"
+#include "inc/hw_memmap.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/gpio.h"
+
+/*
+ * Defines
+ */
+
+
+/*
+ * Global Variable
+ */
+
+
+/*
+ * Prototypes of Functions
+ */
+
+
+/*
+ * main code
+ */
+int main(void)
+{
+	return 0;
+}
